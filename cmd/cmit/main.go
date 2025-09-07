8458d1e64440ee617973afcc4cf8ae2edf6701f8
smokeeaasd <lucasalencargomes111@gmail.com>
2025-09-06 23:42:10 -0300

    🔧 chore: remove debug print

--- cmd/cmit/main.go
+++ cmd/cmit/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/smokeeaasd/cmit/internal/commit"
 	"github.com/smokeeaasd/cmit/internal/form"
-	"github.com/smokeeaasd/cmit/internal/version"
 )
 
 func main() {
-	fmt.Println(version.Version)
 	form := form.CreateForm()
 
 	if err := form.Run(); err != nil {
