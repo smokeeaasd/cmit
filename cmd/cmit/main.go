0010973b701f38dbdcff099217f07c6269b95820
smokeeaasd <lucasalencargomes111@gmail.com>
2025-09-06 23:37:27 -0300

    🐞 fix: set ldflags to use Version variable from package version

--- cmd/cmit/main.go
+++ cmd/cmit/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/smokeeaasd/cmit/internal/commit"
 	"github.com/smokeeaasd/cmit/internal/form"
+	"github.com/smokeeaasd/cmit/internal/version"
 )
 
 func main() {
+	fmt.Println(version.Version)
 	form := form.CreateForm()
 
 	if err := form.Run(); err != nil {
